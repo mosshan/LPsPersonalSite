d5e1d0bed5334d522e4335eb8a5d6e928efb66d7
mosshan <mosshan@yahoo.com>
2022-07-03 12:10:25 -0400

    All project page material added + formatted

--- components/projects.js
+++ components/projects.js
@@ -13,6 +13,21 @@ class Projects extends HTMLElement {
             </div>
             <div class="project-title">Digital Dessert Designer</div>
           </a>
+
+          <a class="project" href="/projectPages/interactiveChess/">
+            <div class="project-image">
+              <img src="/images/chessProject.jpg"  dimensions="1333x1730">
+            </div>
+            <div class="project-title">Interactive Chess Board</div>
+          </a>
+          
+          <a class="project" href="/projectPages/fluxFlow/">
+            <div class="project-image">
+              <img src="/images/fluxFlow.jpg"  dimensions="2000x1333">
+            </div>
+            <div class="project-title">flux flow</div>
+          </a>
+
           <a class="project" href="/projectPages/solidworkPrints/">
             <div>
               <div class="project-image">
@@ -21,6 +36,25 @@ class Projects extends HTMLElement {
               <div class="project-title">solidwork prints</div>
             </div>
           </a>
+
+          <a class="project" href="/projectPages/blanketClothes/">
+            <div>
+              <div class="project-image">
+                <img src="/images/blanketClothes.jpg"  dimensions="1333x2000" >
+              </div>
+              <div class="project-title">blanket clothes</div>
+            </div>
+          </a>
+          
+          <a class="project" href="/projectPages/bb8Modeling/">
+            <div>
+              <div class="project-image">
+                <img src="/images/bb8.png"  dimensions="984x766" >
+              </div>
+              <div class="project-title">modeling bb8 mechanism</div>
+            </div>
+          </a>
+
           <a class="project" href="/projectPages/mazeRacing/">
             <div>
               <div class="project-image">
@@ -29,6 +63,25 @@ class Projects extends HTMLElement {
               <div class="project-title">Robot Maze Racing</div>
             </div>
           </a>
+          
+          <a class="project" href="/projectPages/dogDrawing/">
+            <div>
+              <div class="project-image">
+                <img src="/images/dogDrawing.jfif"  dimensions="2500x1875" >
+              </div>
+              <div class="project-title">Dog Drawing</div>
+            </div>
+          </a>
+          
+          <a class="project" href="/projectPages/surfNTurf/">
+            <div>
+              <div class="project-image">
+                <img src="/images/surfTurf.jfif"  dimensions="2500x1875" >
+              </div>
+              <div class="project-title">surf n' turf</div>
+            </div>
+          </a>
+          
           <a class="project" href="/projectPages/stageDesign/">
             <div>
               <div class="project-image">
@@ -38,28 +91,6 @@ class Projects extends HTMLElement {
             </div>
           </a> 
 
-          <a class="project" href="/projectPages/interactiveChess/">
-            <div class="project-image">
-              <img src="/images/chessProject.jpg"  dimensions="1333x1730">
-            </div>
-            <div class="project-title">Interactive Chess Board</div>
-          </a>
-          <a class="project" href="/projectPages/blanketClothes/">
-            <div>
-              <div class="project-image">
-                <img src="/images/blanketClothes.jpg"  dimensions="1333x2000" >
-              </div>
-              <div class="project-title">blanket clothes</div>
-            </div>
-          </a>
-          <a class="project" href="/projectPages/dogDrawing/">
-            <div>
-              <div class="project-image">
-                <img src="/images/dogDrawing.jfif"  dimensions="2500x1875" >
-              </div>
-              <div class="project-title">Dog Drawing</div>
-            </div>
-          </a>
           <a class="project" href="/projectPages/efficientSolar/">
             <div>
               <div class="project-image">
@@ -69,28 +100,6 @@ class Projects extends HTMLElement {
             </div>
           </a> 
 
-          <a class="project" href="/projectPages/fluxFlow/">
-            <div class="project-image">
-              <img src="/images/fluxFlow.jpg"  dimensions="2000x1333">
-            </div>
-            <div class="project-title">flux flow</div>
-          </a>
-          <a class="project" href="/projectPages/bb8Modeling/">
-            <div>
-              <div class="project-image">
-                <img src="/images/bb8.png"  dimensions="984x766" >
-              </div>
-              <div class="project-title">modeling bb8 mechanism</div>
-            </div>
-          </a>
-          <a class="project" href="/projectPages/surfNTurf/">
-            <div>
-              <div class="project-image">
-                <img src="/images/surfTurf.jfif"  dimensions="2500x1875" >
-              </div>
-              <div class="project-title">surf n' turf</div>
-            </div>
-          </a>
           <a class="project" href="/projectPages/stageDesignMOH/">
             <div>
               <div class="project-image">
