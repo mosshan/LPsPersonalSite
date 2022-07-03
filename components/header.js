0ab76fc96b7db70cf8aa01fa9511cec4fcbf3774
mosshan <mosshan@yahoo.com>
2022-07-03 14:29:44 -0400

    Implemented to work with Github pages
    Removed Jquery and substituted with JS

--- components/header.js
+++ components/header.js
@@ -1,13 +1,3 @@
-
-// Make current link marked in different color
-$(document).ready(function() {
-  $("[href]").each(function() {
-      if (this.href == window.location.href && ($(this).hasClass("mobileLink") || $(this).hasClass("deskLink")) ) {
-          $(this).addClass("active-link");
-      }
-  });
-});
-
 class Header extends HTMLElement {
   constructor() {
     super();
@@ -16,17 +6,17 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div id="canvas">
-      <div id="mobileNav">
+      <div id="mobileNav" >
         <div id="mobileNavWrapper" class="wrapper" style="height: 0px; margin-bottom: 10px;">
           <nav class="main-nav mobileNav" id="mobileLinks">
             <ul>
-              <li id="baseLink"><a href="/" class="mobileLink">WORK</a></li>
-              <li id="blogLink"><a href="/blog/post1/index.html" class="mobileLink">PERSONAL INTERESTS</a></li>
-              <li id="aboutLink"><a href="/about/index.html" class="mobileLink">ABOUT</a></li>
+              <li id="baseLink"><a href="/" class="mobileLink link">WORK</a></li>
+              <li id="blogLink"><a href="/blog/post1/index.html" class="mobileLink link">PERSONAL INTERESTS</a></li>
+              <li id="aboutLink"><a href="/about/index.html" class="mobileLink link">ABOUT</a></li>
             </ul>
           </nav>
         </div>
-        <div id="mobileMenuLink"><a href= javascript:void(0); onclick="OpenMenu()" >Menu</a></div>
+        <div id="mobileMenuLink"><a href= javascript:void(0); onclick="OpenMenu(); highlightActive()"  >Menu</a></div>
       </div>
     
       <header id="header">
@@ -37,9 +27,9 @@ class Header extends HTMLElement {
         <div id="topNav" data-content-field="navigation">
           <nav class="main-nav desktopNav">
             <ul>
-              <li id="baseLink"><a href="/" class="deskLink" >WORK</a></li>
-              <li id="blogLink"><a href="/blog/post1/index.html" class="deskLink">PERSONAL INTERESTS</a></li>
-              <li id="aboutLink"><a href="/about/index.html" class="deskLink">ABOUT</a></li>
+              <li id="baseLink"><a href="/" class="deskLink link" >WORK</a></li>
+              <li id="blogLink"><a href="/blog/post1/index.html" class="deskLink link">PERSONAL INTERESTS</a></li>
+              <li id="aboutLink"><a href="/about/index.html" class="deskLink link">ABOUT</a></li>
             </ul>
           </nav>
         </div>
@@ -64,6 +54,22 @@ function OpenMenu() {
     mobNav.style.height = "0px";
     mobNav.style.borderBottom = "0px solid #ddd"
     mobNav.style.marginBottom = "10px";
+  }
+}
+
+function highlightActive() {
+  var links = document.getElementsByClassName("link");
+  /*
+  if(mobNav.style.display === "none"){
+    var links = document.getElementsByClassName("deskLink");
+    alert("here");
+  } else{
+    var links = document.getElementsByClassName("mobileLink");
+  }*/
+  for(var i = 0, max = links.length; i < max; i++){
+    if(links[i].href == window.location.href && (links[i].classList.contains("mobileLink") || links[i].classList.contains("deskLink") )){
+      links[i].classList.add("active-link");
+    }
   }
 }
 
